e5b6aa7bf405dd5c370438902343c335223c9b81
ushic <ushic@yahoo.com>
2025-01-07 16:06:01 +0330

    Delete src directory

--- src/utils/registration.js
+++
@@ -1,7 +0,0 @@
-/**
- * Generates a random 6-digit registration code
- * @returns {string} A 6-digit code
- */
-export function generateRegistrationCode() {
-  return Math.floor(100000 + Math.random() * 900000).toString()
-}
